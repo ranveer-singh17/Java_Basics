fada8e6fbf666aca96fe2a6f4aa6b8aedd92550d
Ranveer Singh <93751566+ranveer-singh17@users.noreply.github.com>
2022-10-19 22:17:09 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Ogrenci p1 = new Ogrenci("sdfjhs","esdfjs","01234567890","x@gmail.com",true);
+
+		System.out.println(p1.toString());
+	}
+
+}
