a3ba729605a8364a8bb82cb319247cf8b216602d
Ranveer Singh <93751566+ranveer-singh17@users.noreply.github.com>
2022-10-19 22:27:20 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,11 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		Ogrenci p1 = new Ogrenci("sdfjhs","esdfjs","01234567890","x@gmail.com",true);
 
-		System.out.println(p1.toString());
+		System.out.println("It's finally printing");
 	}
 
 }
